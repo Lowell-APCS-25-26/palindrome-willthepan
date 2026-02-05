ba26798021cb700dc2fcef1b9b1b1746d201b2b8
willthepan <williampan.us@gmail.com>
2026-02-05 07:01:37 +0000

    final

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,31 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+
+        String l = s.toLowerCase();
+        String m = "";
+        String t = "";
+
+        for (int i = l.length() - 1; i >= 0; i--) {
+            if (l.charAt(i) != ' ' && Character.isLetter(l.charAt(i))) {
+                t = t + l.charAt(i);
+            }
+        }
+
+        for (int i = 0; i < l.length(); i++) {
+            if (l.charAt(i) != ' ' && Character.isLetter(l.charAt(i))) {
+                m = m + l.charAt(i);
+            }
+        }
+
+        return t.equals(m);
     }
+
     public String reverse(String s) {
-        String n = new String();
-        // your code here
+        String n = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            n = n + s.charAt(i);
+        }
         return n;
     }
 }
